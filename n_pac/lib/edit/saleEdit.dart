0b78d895d2c2b8e0d2957efdb282f9b454ad5328
Piriyaphong <mr.gim@icloud.com>
2020-01-28 22:23:40 +0700

    Update Sale System
    Added Update and Delete Function to system

--- n_pac/lib/edit/saleEdit.dart
+++ n_pac/lib/edit/saleEdit.dart
@@ -1,74 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:n_pac/edit/saleEdit.dart';
 
-class Sale extends StatefulWidget {
+class EditSale extends StatefulWidget {
+  EditSale({this.cusName,this.cusAds,this.valueCement,this.totalPrice,this.deliPrice,this.stoneType,this.cementType,this.index});
+  String cementType ;
+  String stoneType;
+  String cusName ;
+  String cusAds ;
+  int deliPrice ;
+  int valueCement ; 
+  int totalPrice ;
+  final index;
   @override
-  _SaleState createState() => _SaleState();
+  _EditSaleState createState() => _EditSaleState();
 }
 
-class _SaleState extends State<Sale> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        centerTitle: true,
-        title: Text('Sale',style: TextStyle(color: Colors.white),),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))
-        ),
-        
-      ),
-      body: StreamBuilder(
-        stream: Firestore.instance.collection('salebill').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return new Container(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-          return new BillList(
-            document: snapshot.data.documents,
-          );
-        },
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.redAccent,
-        child: Icon(Icons.add,color: Colors.white,),
-        onPressed: (){
-          Navigator.of(context).push(new MaterialPageRoute(builder: (context)=>AddSale()));
-        },
-      ),
-
-    );
-  }
-}
-
-
-
-class AddSale extends StatefulWidget {
-  @override
-  _AddSaleState createState() => _AddSaleState();
-}
-
-class _AddSaleState extends State<AddSale> {
-  String cementType = '';
-  String stoneType = '';
-  String cusName = '';
-  String cusAds = '';
+class _EditSaleState extends State<EditSale> {
+  TextEditingController controllercusName;
+  TextEditingController controllercusAds;
+  TextEditingController controllercementType;
+  TextEditingController controllerstoneType;
+  TextEditingController controllerdeliPrice;
+  TextEditingController controllervalueCement;
+  TextEditingController controllertotalPrice;
+  String cementType ;
+  String stoneType;
+  String cusName ;
+  String cusAds ;
   int deliPrice ;
   int valueCement ; 
   int totalPrice ;
 
-  void addNewBill(){
+  void _editBill(){
     Firestore.instance.runTransaction((Transaction transaction) async {
-      CollectionReference reference = Firestore.instance.collection('salebill');
-      await reference.add({
-        "cusName" : cusName,
+      DocumentSnapshot snapshot = await transaction.get(widget.index);
+      await transaction.update(snapshot.reference, {
+         "cusName" : cusName,
         "cusAds" : cusAds,
         "cementType" : cementType,
         "stoneType" : stoneType,
@@ -76,23 +43,60 @@ class _AddSaleState extends State<AddSale> {
         "valueCement" : valueCement,
         "totalPrice" : totalPrice,
         "timeStamp" : DateTime.now(),
-
       });
     });
     Navigator.pop(context);
   }
 
-  
+  void _deleteBill() {
+    Firestore.instance.runTransaction((Transaction transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(widget.index);
+      await transaction.delete(widget.index);
+    }); 
+    Navigator.pop(context);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    cementType = widget.cementType;
+    stoneType = widget.stoneType;
+    cusName = widget.cusName;
+    cusAds = widget.cusAds;
+    deliPrice = widget.deliPrice;
+    valueCement = widget.valueCement;
+    totalPrice = widget.totalPrice;
+    controllercusName = new TextEditingController(text: widget.cusName);
+    controllercusAds = new TextEditingController(text: widget.cusAds);
+    controllercementType = new TextEditingController(text: widget.cementType);
+    controllerdeliPrice = new TextEditingController(text: widget.deliPrice.toString());
+    controllertotalPrice = new TextEditingController(text: widget.totalPrice.toString());
+    controllerstoneType = new TextEditingController(text: widget.stoneType);
+    controllervalueCement = new TextEditingController(text: widget.valueCement.toString());
+
+    
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+       appBar: AppBar(
          backgroundColor: Colors.redAccent,
         centerTitle: true,
-        title: Text('NEW BILL',style: TextStyle(color: Colors.white),),
+        title: Text('EDIT BILL',style: TextStyle(color: Colors.white),),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))
         ),   
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete,color:Colors.white),
+            onPressed: (){
+              _deleteBill();
+            },
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -100,6 +104,7 @@ class _AddSaleState extends State<AddSale> {
           child: Column(
             children: <Widget>[
               TextField(
+                controller: controllercusName,
                 decoration: InputDecoration(
                   labelText: 'ชื่อลูกค้า',
                   alignLabelWithHint: true              
@@ -111,6 +116,7 @@ class _AddSaleState extends State<AddSale> {
                 },
               ),
               TextField(
+                controller: controllercusAds,
                 decoration: InputDecoration(
                   labelText: 'ที่อยู่ลูกค้า',
                   alignLabelWithHint: true
@@ -122,6 +128,7 @@ class _AddSaleState extends State<AddSale> {
                 },
               ),
               TextField(
+                controller: controllervalueCement,
                 decoration: InputDecoration(
                   labelText: 'จำนวนคิว',
                   alignLabelWithHint: true,
@@ -134,6 +141,7 @@ class _AddSaleState extends State<AddSale> {
                 },
               ),
               TextField(
+                controller: controllerdeliPrice,
                 decoration: InputDecoration(
                   labelText: 'ค่าขนส่ง',
                   alignLabelWithHint: true,
@@ -228,6 +236,7 @@ class _AddSaleState extends State<AddSale> {
                 ),
               ),
               TextField(
+                controller: controllertotalPrice,
                 decoration: InputDecoration(
                   labelText: 'รวมเงิน',
                   alignLabelWithHint: true,
@@ -235,7 +244,7 @@ class _AddSaleState extends State<AddSale> {
                 keyboardType: TextInputType.number,
                 onChanged: (input){
                   setState(() {
-                    totalPrice = num.tryParse(input) ;
+                    totalPrice = num.tryParse(input);
                   });
                 },
               ),
@@ -245,95 +254,13 @@ class _AddSaleState extends State<AddSale> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.redAccent,
-        child: Icon(Icons.save,color: Colors.white,),
+        child: Icon(Icons.save,color: Colors.white),
         onPressed: (){
-          addNewBill();
+         _editBill();
         },
       ),    
-    );
-  }
-}
-
-
-
-class BillList extends StatelessWidget {
-  List<DocumentSnapshot> document;
-  BillList({this.document});
-
-  @override
-  Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemCount: document.length,
-      itemBuilder: (BuildContext context, int i ){
-        String cusName = document[i].data['cusName'];
-        String cusAds = document[i].data['cusAds'];
-        int deliPrice = document[i].data['deliPrice'];
-        int valueCement = document[i].data['valueCement'];
-        int totalPrice = document[i].data['totalPrice'];
-        String cementType = document[i].data['cementType'];
-        String stoneType = document[i].data['stoneType'];
-        return Padding(
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: <Widget>[
-              new Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius:5,
-                      spreadRadius: 1,
-                    )]
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(cusName),
-                      Container(height: 10,),
-                      Text(cusAds),
-                      Row(children: <Widget>[
-                        Text(valueCement.toString()),
-                        Text('  คิว  ค่าขนส่ง    '),
-                        Text(deliPrice.toString()),
-                        Text('  บาท     รวม    '),
-                        Text(totalPrice.toString()),
-                        Text('  บาท     '),
-                      ],),
-                      Row(children: <Widget>[
-                        Text(cementType),
-                        Container(width: 20,),
-                        Text(stoneType),
-                      ],)
-
-                    ],
-                  ),
-                ),
-              ),
-              new IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: (){
-                  Navigator.of(context).push(new MaterialPageRoute(builder: (context) => EditSale(
-                    cusName : cusName,
-                    cusAds : cusAds,
-                    deliPrice : deliPrice,
-                    valueCement : valueCement,
-                    totalPrice : totalPrice,
-                    cementType : cementType,
-                    stoneType : stoneType,
-                    index : document[i].reference
-
-                  )));
-                },
-              )
-            ],
-          ),
-        );
-
-      },
-
+   
+      
     );
   }
 }
