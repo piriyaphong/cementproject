d75103034493ab9621cb46a98281868e1a042b79
Piriyaphong <mr.gim@icloud.com>
2020-01-29 15:43:43 +0700

    Update main.dart
    Add assets image and add image backgroud to container buttom

--- n_pac/lib/main.dart
+++ n_pac/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -30,13 +31,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('N-PAC',style: TextStyle(color: Colors.white),),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))
+        title: Text(
+          'N-PAC',
+          style: TextStyle(color: Colors.white),
         ),
-        
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+        // flexibleSpace: Image(
+        //   image: AssetImage('assets/images/bgred.PNG'),
+        //   fit: BoxFit.cover,
+        // ),
+        // backgroundColor: Colors.transparent,
       ),
-
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(20),
@@ -46,12 +52,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.all(1),
                 child: Container(
                   alignment: Alignment.centerLeft,
-                  child: Text('Welcome',style: TextStyle(
-                    fontSize: 50,                   
-                    shadows:[
-                      Shadow(blurRadius: 20,color: Colors.black38,offset: Offset(5.0, 5.0),)
-                    ]
-                  ),),
+                  child: Text(
+                    'Welcome',
+                    style: TextStyle(fontSize: 50, shadows: [
+                      Shadow(
+                        blurRadius: 20,
+                        color: Colors.black38,
+                        offset: Offset(5.0, 5.0),
+                      )
+                    ]),
+                  ),
                 ),
               ),
               Padding(
@@ -60,19 +70,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    )]
-                  ),
+                      //color: Colors.redAccent,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bgred.PNG'),
+                          fit: BoxFit.fill),
+                      borderRadius: new BorderRadiusDirectional.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                        )
+                      ]),
                   child: ListTile(
-                    leading: Icon(Icons.shopping_cart,size: 50,color: Colors.white,),
-                    title: Text('SALE',style: TextStyle(fontSize: 50,color: Colors.white),),
-                    onTap: (){
-                      Navigator.of(context).push(new MaterialPageRoute(builder: (context)=>Sale()));
+                    leading: Icon(
+                      Icons.shopping_cart,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'SALE',
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                          new MaterialPageRoute(builder: (context) => Sale()));
                     },
                   ),
                 ),
@@ -83,19 +105,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.pinkAccent,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    )]
-                  ),
+                      //color: Colors.pinkAccent,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bgpink.PNG'),
+                          fit: BoxFit.fill),
+                      borderRadius: new BorderRadiusDirectional.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                        )
+                      ]),
                   child: ListTile(
-                    leading: Icon(Icons.attach_money,size: 50,color: Colors.white,),
-                    title: Text('MONEY',style: TextStyle(fontSize: 50,color: Colors.white),),
-                    onTap: (){
-                      Navigator.of(context).push(new MaterialPageRoute(builder: (context)=> Money()));
+                    leading: Icon(
+                      Icons.attach_money,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'MONEY',
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                          new MaterialPageRoute(builder: (context) => Money()));
                     },
                   ),
                 ),
@@ -106,19 +140,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.orangeAccent,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    )]
-                  ),
+                      //color: Colors.orangeAccent,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bgorange.PNG'),
+                          fit: BoxFit.fill),
+                      borderRadius: new BorderRadiusDirectional.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                        )
+                      ]),
                   child: ListTile(
-                    leading: Icon(Icons.person,size: 50,color: Colors.white,),
-                    title: Text('PERSONEL',style: TextStyle(fontSize: 50,color: Colors.white),),
-                    onTap: (){
-                      Navigator.of(context).push(new MaterialPageRoute(builder: (context) => Personel()));
+                    leading: Icon(
+                      Icons.person,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'PERSONEL',
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(new MaterialPageRoute(
+                          builder: (context) => Personel()));
                     },
                   ),
                 ),
@@ -129,19 +175,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    )]
-                  ),
+                      //color: Colors.yellow,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bgyellow.PNG'),
+                          fit: BoxFit.fill),
+                      borderRadius: new BorderRadiusDirectional.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                        )
+                      ]),
                   child: ListTile(
-                    leading: Icon(Icons.drive_eta,size: 50,color: Colors.white,),
-                    title: Text('CAR',style: TextStyle(fontSize: 50,color: Colors.white),),
-                    onTap: (){
-                      Navigator.of(context).push(new MaterialPageRoute(builder: (context) => Car()));
+                    leading: Icon(
+                      Icons.drive_eta,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'CAR',
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                          new MaterialPageRoute(builder: (context) => Car()));
                     },
                   ),
                 ),
@@ -152,17 +210,28 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    )]
-                  ),
+                      //color: Colors.green,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bggreen.PNG'),
+                          fit: BoxFit.fill),
+                      borderRadius: new BorderRadiusDirectional.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                        )
+                      ]),
                   child: ListTile(
-                    leading: Icon(Icons.local_gas_station,size: 50,color: Colors.white,),
-                    title: Text('PETRO',style: TextStyle(fontSize: 50,color: Colors.white),),
+                    leading: Icon(
+                      Icons.local_gas_station,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'PETRO',
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
@@ -172,26 +241,35 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.lightBlue,
-                    borderRadius: new BorderRadiusDirectional.circular(10),
-                    boxShadow: [BoxShadow(
-                      color: Colors.black45,
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    )]
-                  ),
+                      //color: Colors.lightBlue,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bgblue.PNG'),
+                          fit: BoxFit.fill),
+                      borderRadius: new BorderRadiusDirectional.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                        )
+                      ]),
                   child: ListTile(
-                    leading: Icon(Icons.store_mall_directory,size: 50,color: Colors.white,),
-                    title: Text('STOCK',style: TextStyle(fontSize: 50,color: Colors.white),),
+                    leading: Icon(
+                      Icons.store_mall_directory,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'STOCK',
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-
             ],
           ),
         ),
       ),
-      
     );
   }
 }
